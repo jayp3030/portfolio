42dde1df6f19d692d3c180140859ddda6166fd42
jay <jayp33233@gmail.com>
2023-06-26 09:49:14 +0530

    leetcode profile added

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,6 +6,7 @@ export default function Social() {
       <div className="social">
         <a href="mailto:jayp46157@gmail.com"><i className="fa-solid fa-envelope"></i></a>
         <a href="https://github.com/jayp3030" target="_blank" rel="noreferrer"><i className="fa-brands fa-github" ></i></a>
+        <a href="https://leetcode.com/jayp3030/" target="_blank" rel="noreferrer"><i class="fa-solid fa-code"></i></a>
         <a href="https://www.linkedin.com/in/jay-n-prajapati-34519620b/" target="_blank" rel="noreferrer" ><i className="fa-brands fa-linkedin"></i></a>
       </div>
     </>
