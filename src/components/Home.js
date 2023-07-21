85707846e0b59fb42e805176df9bb4c08c896ab1
jay <jayp33233@gmail.com>
2023-07-21 10:38:38 +0530

    new project added and resume link updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
         <div className="innerHome" data-aos='fade-down' data-aos-delay="500">
             <h1>hey I'm jay prajapati</h1>
             <p>MERN stack developer creating web applications that deliver exceptional user experiences. My expertise in MongoDB, Express.js, React, and Node.js allows me to build robust and scalable solutions that meet business needs. </p>
-            <a href="https://drive.google.com/file/d/1IoOw3gOg-w2ScfX4JkdYX1y4WGmdv2ve/view?usp=share_link" target="_blank" rel="noreferrer" ><button className='btn-invert'>resume</button></a>
+            <a href="https://drive.google.com/file/d/1TYZGtT7_O0t-vge4jUuw20wLxhy2yeNw/view?usp=sharing" target="_blank" rel="noreferrer" ><button className='btn-invert'>resume</button></a>
             <a href="#projects"><button className='btn'>projects</button></a>
         </div>
       </div>
